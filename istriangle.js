574009ca85e1ce526ca48a8ae7399427a1681046
anandkumarcoder <akanand104@gmail.com>
2021-08-17 20:15:08 +0530

    Is traingle done, intial commit quiz

---
+++ istriangle.js
@@ -0,0 +1,32 @@
+const input = document.querySelectorAll(".angleInput")
+const triangleBtn = document.querySelector("#isTriangleBtn")
+
+const outputDiv = document.querySelector("#output")
+
+
+
+function calculateSumOfAngle(angle1 , angle2 , angle3) {
+    const sumOfAngle = angle1 + angle2 + angle3;
+    return(sumOfAngle)
+}
+
+
+
+
+function isTriangle(){
+    
+    const sumOfAngle = calculateSumOfAngle(Number(input[0].value), Number(input[1].value), Number(input[2].value));
+    
+
+    if(sumOfAngle === 180) {
+        outputDiv.innerText="yes this is a triangle";
+    } else {
+        outputDiv.innerText="no this is not a triangle";
+    }
+    
+}
+
+
+triangleBtn.addEventListener("click", isTriangle)
+    
+
