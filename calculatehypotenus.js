e76f014effc1f6be9e74221474d95dde475f250b
anandkumarcoder <akanand104@gmail.com>
2021-08-19 17:01:28 +0530

    basic logic build is completed

--- calculatehypotenus.js
+++ calculatehypotenus.js
@@ -1,3 +1,22 @@
 const userInput = document.querySelectorAll(".sideInput");
 const calculateBtn = document.querySelector("#calculateHypotenus");
 const outputDiv = document.querySelector("#output")
+
+
+function squareOfSides(a, b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares
+}
+
+
+
+function calculateHypotenus() {
+   const sumOfSquares = squareOfSides(Number(userInput[0].value), Number(userInput[1].value));
+   
+   lengthOfHypotenus = Math.sqrt(sumOfSquares);
+   outputDiv.innerText = "lenght of hypotneus is " + lengthOfHypotenus + "cm"
+
+}
+// variable deinfed here equal to function defined in line 6
+
+calculateBtn.addEventListener("click", calculateHypotenus)
