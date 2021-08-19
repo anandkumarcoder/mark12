b7f1518088cc624af65d885b07ece910995c91e7
anandkumarcoder <akanand104@gmail.com>
2021-08-19 20:18:38 +0530

    structure of js

---
+++ area.js
@@ -0,0 +1,12 @@
+const side1 = document.querySelector("#side1");
+const side2 = document.querySelector("#side2");
+const side3 = document.querySelector("#side3");
+const areaButton =document.querySelector("#areaBtn");
+const outputDiv = document.querySelector("#output");
+
+
+function calculateArea() {
+    console.log("testing")
+}
+
+areaButton.addEventListener("click",calculateArea)
