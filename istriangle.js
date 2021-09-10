f259d465a487285a1b72033b4e6b57362ef67b02
anandkumarcoder <akanand104@gmail.com>
2021-09-10 23:59:05 +0530

    to check empty fields in is traingle added

--- istriangle.js
+++ istriangle.js
@@ -5,28 +5,34 @@ const outputDiv = document.querySelector("#output")
 
 
 
-function calculateSumOfAngle(angle1 , angle2 , angle3) {
+function calculateSumOfAngle(angle1, angle2, angle3) {
     const sumOfAngle = angle1 + angle2 + angle3;
-    return(sumOfAngle)
+    return (sumOfAngle)
 }
 
 
 
 
-function isTriangle(){
-    
-    const sumOfAngle = calculateSumOfAngle(Number(input[0].value), Number(input[1].value), Number(input[2].value));
-    
+function isTriangle() {
 
-    if(sumOfAngle === 180) {
-        outputDiv.innerText="yes this is a triangle";
+    const sumOfAngle = calculateSumOfAngle(Number(input[0].value), Number(input[1].value), Number(input[2].value));
+
+
+    if (sumOfAngle === 0 || sumOfAngle < 0) {
+        outputDiv.innerText = "please put in valid angles"
     } else {
-        outputDiv.innerText="no this is not a triangle";
+        if (sumOfAngle === 180) {
+            outputDiv.innerText = "yes this is a triangle";
+        } else {
+            outputDiv.innerText = "no this is not a triangle";
+        }
+
     }
-    
+
+ 
+
+
 }
 
 
 triangleBtn.addEventListener("click", isTriangle)
-    
-
